f73788a5e6abc1bfa34eda52b1433d64b89317f6
atta <atta@gmail.com>
2026-02-06 19:05:16 +0300

    Creat Class Patient.cs

---
+++ Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hospitalSystem
+{
+    internal class Patient : Persone
+    {
+        public int IdPatient{ get; set; }
+
+        public Patient(string name, int age, string gender) : base(name, age, gender)
+        {
+            IdPatient = ID;
+
+        }
+        
+        public override void printData()
+        {
+            Console.WriteLine($"this IdPatient : {ID} \nthis Nmae patint : {Name} \nthis Age patint :{Age} \nthis is Gender patint : {Gender} ");
+            Console.WriteLine("----------------------------------------------------------");
+        }
+        
+    }
+}

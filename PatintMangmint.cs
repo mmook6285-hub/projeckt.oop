f9bebde16e6e4e038d9e378ff9b10d58eb16444b
atta <atta@gmail.com>
2026-02-06 19:07:01 +0300

    Craet Class PatintMangmint for mangmint Patient

---
+++ PatintMangmint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hospitalSystem
+{
+    internal class PatintMangmint
+    {
+        private List<Patient> patients;
+        public PatintMangmint()
+        {
+            patients = new List<Patient>();
+        }
+        public void AddPatient(Patient patient) {
+            patients.Add(patient);
+        }
+
+        public void DelForName(string name ) {
+
+            patients.RemoveAll(p => p.Name == name );
+
+        }
+
+        // Show Data
+        public void GetData() {
+
+            foreach (Patient pat in patients) {
+
+                Console.WriteLine($"name patint is : {pat.Name} \nAge patint is {pat.Age} \nGender patint is :{pat.Gender}");
+
+            }        }
+
+        // Find Patient By Name
+        public Patient FindPatintForName(string name) {
+
+            return patients.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}

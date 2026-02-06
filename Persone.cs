80bf42c87ad283f6d06d21c661537697edde54e0
atta <atta@gmail.com>
2026-02-06 16:49:23 +0300

    creat mithod PrintData

---
+++ Persone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospitalSystem
+{
+    internal class Persone
+    {
+        private  int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        static int  id = 0;
+        //Creat Constrctor
+        public Persone(string name , int age) {
+            id++;
+            ID=id;
+            Name= name;
+            Age = age;
+        }
+        public void printData() {
+            Console.WriteLine($"this Id : {ID} \nthis Nmae : {Name} \nthis Age :{Age}");
+            Console.WriteLine("----------------------------------------------------------");
+        
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("dwa");
+            Persone p1 =new Persone("atta",25);
+            Persone p2 = new Persone("mody",13);
+            Persone p3 = new Persone("mahmoud",22);
+            Persone p4 = new Persone("mahmoud",22);
+            Persone p5 = new Persone("mahmoud",22);
+            Persone p6 = new Persone("mahmoud",22);
+            p1.printData();
+            p2.printData();
+            p3.printData();
+            p4.printData();
+            p5.printData();
+            p6.printData();
         }
     }
 }

8ef27d0d0dfbc764e019d0ed2fc107b7354e78f2
mody <mody@gmail.com>
2026-02-06 17:27:17 +0300

    added in class person Gender

--- Persone.cs
+++ Persone.cs
@@ -11,16 +11,20 @@ namespace hospitalSystem
         private  int ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Gender { get; set; }
         static int  id = 0;
         //Creat Constrctor
-        public Persone(string name , int age) {
+        public Persone(string name , int age, string gender)
+        {
             id++;
-            ID=id;
-            Name= name;
+            ID = id;
+            Name = name;
             Age = age;
+            Gender = gender;
+            
         }
         public void printData() {
-            Console.WriteLine($"this Id : {ID} \nthis Nmae : {Name} \nthis Age :{Age}");
+            Console.WriteLine($"this Id : {ID} \nthis Nmae : {Name} \nthis Age :{Age} \nthis is Gender : {Gender}");
             Console.WriteLine("---------------------------g-------------------------------");
         
         }
--- Program.cs
+++ Program.cs
@@ -4,18 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Persone p1 =new Persone("atta",25);
-            Persone p2 = new Persone("mody",13);
-            Persone p3 = new Persone("mahmoud",22);
-            Persone p4 = new Persone("mahmoud",22);
-            Persone p5 = new Persone("mahmoud",22);
-            Persone p6 = new Persone("mahmoud",22);
+            Persone p1 =new Persone("atta",25, "male");
+            Persone p2 = new Persone("mody",13,"femal");
+         
             p1.printData();
             p2.printData();
-            p3.printData();
-            p4.printData();
-            p5.printData();
-            p6.printData();
+         
         }
     }
 }

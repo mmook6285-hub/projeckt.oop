9c55b4f5a722bc0ea0a5cdfe8531625390df1930
mody <mody@gmail.com>
2026-02-07 17:25:33 +0300

    اضفت كلاس Date & Id

---
+++ Appointment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospitalSystem
+{
+    internal class Appointment
+    {
+        //خصاءص
+        public int Id { get; private set; }
+        public int PatientId { get; private set; }
+        public int DoctorId { get; private set; }
+        public  DateTime Date { get;private set; }
+    //constructor
+    public  Appointment(int id, int patientId, int doctorId,  DateTime date){
+        Id = id;
+        PatientId = patientId;
+        DoctorId = doctorId;
+        Date = date;
+}
+//Update Date
+public void UpdateDate(DateTime newDate)
+{
+    Date = newDate;
+}
+        //PrintDate
+        public void printData()
+        {
+            Console.WriteLine("Appointment   Info:");
+            Console.WriteLine($"Id         :     {Id}");
+            Console.WriteLine($"PatientId  :     {PatientId}");
+            Console.WriteLine($"DoctorId   :    {DoctorId}");
+            Console.WriteLine($"Date       :    {Date}");
+            Console.WriteLine("-----------------------------------");
+
+        }
+    }
+}
